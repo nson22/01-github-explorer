1971209cfe8b3b8c23ee998ff5acc64a10f95a9b
Gglecinilson Silva <glecinilson@gmail.com>
2021-03-09 21:18:28 -0400

    Refactoring and add Typescript

--- src/components/RepositoryList.tsx
+++ src/components/RepositoryList.tsx
@@ -3,8 +3,14 @@ import { RepositoryItem } from './RepositoryItem'
 
 import '../styles/repositories.scss'
 
+interface Repository {
+  name: string;
+  description: string;
+  html_url: string;
+}
+
 export function RepositoryList() {
-  const [repositories, setRepositories] = useState([])
+  const [repositories, setRepositories] = useState<Repository[]>([])
 
   useEffect(() => {
     fetch(`https://api.github.com/orgs/rocketseat/repos`)
@@ -14,7 +20,7 @@ export function RepositoryList() {
 
   return (
     <section className="repository-list">
-      <h1>Repository List</h1>
+      <h1>Repository List:</h1>
       <ul>
         {repositories.map(repository => {
           return <RepositoryItem key={repository.name} repository={repository}/>
